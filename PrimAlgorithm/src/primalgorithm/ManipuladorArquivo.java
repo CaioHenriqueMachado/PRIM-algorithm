846d6c042b810680e07fd6337de2fb52984c5a92
Caio <caio.student@outlook.com>
2020-10-31 15:27:53 -0300

    ajuste do path

--- PrimAlgorithm/src/primalgorithm/ManipuladorArquivo.java
+++ PrimAlgorithm/src/primalgorithm/ManipuladorArquivo.java
@@ -14,95 +14,96 @@ public class ManipuladorArquivo {
     static Boolean Erro = false;
     
     public static void coletarDados() {
-    File file = new File("C:/Users/caiom/Git/PRIM-algorithm/grafo.txt");
-    Scanner text = null;
-    Integer line_number = 0;
-    String nomeVertices = "";
-    Integer qtd_vertice = 0; 
-    String[] strToken;
-    Configuracao config = new Configuracao();
+        
+        File file = new File(System.getProperty("user.dir") + "\\grafo.txt");
+        Scanner text = null;
+        Integer line_number = 0;
+        String nomeVertices = "";
+        Integer qtd_vertice = 0; 
+        String[] strToken;
+        Configuracao config = new Configuracao();
 
-    try{
-        text = new Scanner(file);
+        try{
+            text = new Scanner(file);
 
-        while(text.hasNextLine()) {
-            Vertice vertices = new Vertice();
-            Aresta aresta = new Aresta();
-            String line = text.nextLine();
-            line_number++;
-            
-            if(line.isEmpty()){
-                System.out.println("ERRO:GRAFO COM LINHA EM BRANCO!!");
-                Erro = true;
-                break;
-            }
-            
-            if (line_number == 1){
-                switch(line){
-                    case "1":
-                        System.out.println("Grafo direcionado !!");
-                        config.Direcionado = true;
-                        break;
-                    case "2":
-                        System.out.println("Grafo direcionado !!");
-                        config.Direcionado = false;
-                        break;
-                    default:
-                        System.out.println("ERRO: TIPO DE GRAFO INVÁLIDO !!");
-                        Erro = true;
-                        break;
-                }
-            
-            }
-            
-            if (line_number == 2){
-                qtd_vertice = Integer.parseInt(line);
-            }
-            
-            //Nome dos vertices
-            if (line_number > 2 && line_number <= (2 + qtd_vertice) ){
-                vertices.setNome(line);
-                ArrayVertices.add(vertices);
-                
-                nomeVertices+= line + ","; 
-            }
-            
-            //Arestas
-            if ( line_number > (2 + qtd_vertice) ){
-                if (line.split(",").length == 3){
-                    strToken = line.trim().split(",");
-                    aresta.setPesoAresta(Integer.parseInt(strToken[2]));
-                }else {
-                    System.out.println("ERRO: ARESTA SEM CUSTO !!");
+            while(text.hasNextLine()) {
+                Vertice vertices = new Vertice();
+                Aresta aresta = new Aresta();
+                String line = text.nextLine();
+                line_number++;
+
+                if(line.isEmpty()){
+                    System.out.println("ERRO:GRAFO COM LINHA EM BRANCO!!");
                     Erro = true;
                     break;
                 }
-                aresta.setVerticeOrigem(strToken[0]);
-                aresta.setVerticeDestino(strToken[1]);
-                ArrayPesosArestas.add(aresta);
+
+                if (line_number == 1){
+                    switch(line){
+                        case "1":
+                            System.out.println("Grafo direcionado !!");
+                            config.Direcionado = true;
+                            break;
+                        case "2":
+                            System.out.println("Grafo direcionado !!");
+                            config.Direcionado = false;
+                            break;
+                        default:
+                            System.out.println("ERRO: TIPO DE GRAFO INVÁLIDO !!");
+                            Erro = true;
+                            break;
+                    }
+
+                }
+
+                if (line_number == 2){
+                    qtd_vertice = Integer.parseInt(line);
+                }
+
+                //Nome dos vertices
+                if (line_number > 2 && line_number <= (2 + qtd_vertice) ){
+                    vertices.setNome(line);
+                    ArrayVertices.add(vertices);
+
+                    nomeVertices+= line + ","; 
+                }
+
+                //Arestas
+                if ( line_number > (2 + qtd_vertice) ){
+                    if (line.split(",").length == 3){
+                        strToken = line.trim().split(",");
+                        aresta.setPesoAresta(Integer.parseInt(strToken[2]));
+                    }else {
+                        System.out.println("ERRO: ARESTA SEM CUSTO !!");
+                        Erro = true;
+                        break;
+                    }
+                    aresta.setVerticeOrigem(strToken[0]);
+                    aresta.setVerticeDestino(strToken[1]);
+                    ArrayPesosArestas.add(aresta);
+                }
+
+
             }
-            
 
+            config.setVerticeInicial(PrimAlgorithm.verticeInicial);
+            ArrayConfig.add(config);
+
+            if (!Erro){
+                vertices_list =  nomeVertices.trim().split(","); 
+                System.out.println("São " + qtd_vertice + " vertices, sendo eles: " + nomeVertices);
+            }
+
+
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
         }
-        
-        config.setVerticeInicial(PrimAlgorithm.verticeInicial);
-        ArrayConfig.add(config);
-        
-        if (!Erro){
-            vertices_list =  nomeVertices.trim().split(","); 
-            System.out.println("São " + qtd_vertice + " vertices, sendo eles: " + nomeVertices);
+
+        text.close();
         }
-        
 
-    } catch(IOException e) {
-        System.out.println(e.getMessage());
-    }
-    
-    text.close();
-    }
-
-    private static void Switch(String line) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        private static void Switch(String line) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
 
 }
